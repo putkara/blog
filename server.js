b57cb8f7aa8dce49a448d6f45773664444bfd3f0
putkara <natia.meskhi.natia@gmail.com>
2020-10-28 13:39:14 +0100

    blog

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./views/articles/models/article')
+const articleRouter = require('./routes/articles')
+const app = express()
+const methodOverride = require('method-override')
+
+mongoose.connect('mongodb://localhost/blog', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex : true
+})
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('articles/index', {
+        articles: articles
+    })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
